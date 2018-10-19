574ccca0c8b5b833525bd32d4571f2fe4a9f99a3
Price Comstock <price.comstock@gmail.com>
2018-10-19 14:12:10 -0500

    got basic home page with join box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <br>
+    <div class="columns is-centered is-mobile">
+      <div class="column is-one-quarter-tablet is-two-thirds-mobile">
+        <div class="field has-addons is-expanded">
+          <div class="control is-expanded">
+            <input class="input is-large is-uppercase" type="text" placeholder="Code">
+          </div>
+          <div class="control">
+            <a class="button is-primary is-large">
+              JOIN
+            </a>
+          </div>
+        </div>
+        <a href="" class="button is-white">CREATE</a>
+      </div>
+    </div>
+    <br>
   </div>
 </template>
 
