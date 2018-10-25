1752e99b3f7a52e7069b952147c8b348ec5ec20e
Price Comstock <price.comstock@gmail.com>
2018-10-25 14:36:29 -0500

    made client socket.io in a way that doesn't need environment variables and will just use the url it's being served from

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import socketio from "socket.io-client";
 import VueSocketio from "vue-socket.io";
 
 // export const SocketInstance = socketio('http://localhost:5000')
-Vue.use(VueSocketio, process.env.VUE_APP_BACKEND_URL);
+Vue.use(VueSocketio, socketio());
 // Vue.use(VueSocketio, SocketInstance);
 
 Vue.config.productionTip = false;
