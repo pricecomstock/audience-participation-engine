38be0e712be04b78107244bbe27fb95c08d3491a
Price Comstock <price.comstock@gmail.com>
2018-10-25 13:57:11 -0500

    added .listen to socketio instantiation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require("express")();
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var io = require("socket.io").listen(http);
 var createRouter = require("./router.js").createRouter;
 var serveStatic = require("serve-static");
 
